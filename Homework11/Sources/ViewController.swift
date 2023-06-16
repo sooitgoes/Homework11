2a53ccca5de9e5ba57284d996ca3a6c57ed98314
Mikhail Laty <laty.mischa@gmail.com>
2023-06-16 17:58:15 +0300

    Change background color.

--- Homework11/Sources/ViewController.swift
+++ Homework11/Sources/ViewController.swift
@@ -4,16 +4,39 @@ class ViewController: UIViewController {
 
     // MARK: - UI Elements
 
+    private lazy var gradient: CAGradientLayer = {
+            let gradient = CAGradientLayer()
+            gradient.frame = view.bounds
+            gradient.colors = [UIColor.systemPurple.cgColor, UIColor.white.cgColor]
+            gradient.startPoint = CGPoint(x: 0.5, y: 0)
+            gradient.endPoint = CGPoint(x: 0.5, y: 1)
+            return gradient
+        }()
+
 
 
     // MARK: - Lifecycle
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        setupView()
 
     }
 
     // MARK: - Setups
+
+    private func setupView() {
+        view.layer.insertSublayer(gradient, at: 0)
+
+    }
+
+    private func setupHierarchy() {
+
+    }
+
+    private func setupLayout() {
+
+    }
 
 
     // MARK: - Actions
