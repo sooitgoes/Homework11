5b49d0024367d85a1631a720ffc0fced9dd867a8
Mikhail Laty <laty.mischa@gmail.com>
2023-06-17 23:06:25 +0300

    Create extension for UIButton.

---
+++ Homework11/Extension/UIButton + Extension.swift
@@ -0,0 +1,10 @@
+import UIKit
+
+extension UIButton {
+    func addRightImage(image: UIImage, offset: CGFloat) {
+        self.setImage(image, for: .normal)
+        self.imageView?.translatesAutoresizingMaskIntoConstraints = false
+        self.imageView?.centerYAnchor.constraint(equalTo: self.centerYAnchor, constant: 0.0).isActive = true
+        self.imageView?.trailingAnchor.constraint(equalTo: self.trailingAnchor, constant: -offset).isActive = true
+    }
+}
