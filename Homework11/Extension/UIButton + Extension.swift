5f5c275c4f7ef7f8ec602d54109bcd70dfe4cddc
Mikhail Laty <laty.mischa@gmail.com>
2023-06-18 15:19:30 +0300

    Change method addRightImage.

--- Homework11/Extension/UIButton + Extension.swift
+++ Homework11/Extension/UIButton + Extension.swift
@@ -1,10 +1,12 @@
 import UIKit
 
 extension UIButton {
-    func addRightImage(image: UIImage, offset: CGFloat) {
+    func addLeftImage(image: UIImage, offset: CGFloat) {
         self.setImage(image, for: .normal)
         self.imageView?.translatesAutoresizingMaskIntoConstraints = false
-        self.imageView?.centerYAnchor.constraint(equalTo: self.centerYAnchor, constant: 0.0).isActive = true
-        self.imageView?.trailingAnchor.constraint(equalTo: self.trailingAnchor, constant: -offset).isActive = true
+        self.imageView?.centerYAnchor.constraint(equalTo: self.centerYAnchor, constant: 0).isActive = true
+        self.imageView?.trailingAnchor.constraint(equalTo: self.leadingAnchor, constant: offset).isActive = true
     }
 }
+
+
