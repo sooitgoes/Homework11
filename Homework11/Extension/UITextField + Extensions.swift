5550b24771d1d36cbcc7e5de537ea6b63d6dd19e
Mikhail Laty <laty.mischa@gmail.com>
2023-06-18 16:54:24 +0300

    Add extension for UITextField.

--- Homework11/Extension/UITextField + Extensions.swift
+++ Homework11/Extension/UITextField + Extensions.swift
@@ -11,4 +11,15 @@ extension UITextField {
     }
 }
 
+extension UITextField {
+    func setRightIcon(_ image: UIImage) {
+        let iconView = UIImageView(frame: CGRect(x: 40, y: 5, width: 20, height: 20))
+        iconView.image = image
+        let iconContainerView: UIView = UIView(frame: CGRect(x: 20, y: 0, width: 80, height: 30))
+        iconContainerView.addSubview(iconView)
+        rightView = iconContainerView
+        rightViewMode = .always
+    }
+}
+
 
